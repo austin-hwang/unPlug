1303d5c9b5c687597205220f088f22e2388f6361
manasi-m <cyber.manasi@gmail.com>
2017-12-05 20:51:19 -0500

    google cloud vision changes

--- scripts/routes.js
+++ scripts/routes.js
@@ -4,8 +4,50 @@ var stripe = require("stripe")(
 var bodyParser = require('body-parser')
 var express = require('express');
 var app = express();
-  
+
 app.use(bodyParser.urlencoded({ extended: false }))
+
+// Imports the Google Cloud client library.
+const Storage = require('@google-cloud/storage');
+
+// Instantiates a client. If you don't specify credentials when constructing
+// the client, the client library will look for credentials in the
+// environment.
+const storage = Storage();
+// Imports the Google Cloud client library
+const Vision = require('@google-cloud/vision');
+
+// Creates a client
+const vision = new Vision();
+
+// Makes an authenticated API request.
+storage
+.getBuckets()
+.then((results) => {
+  const buckets = results[0];
+
+  console.log('Buckets:');
+  buckets.forEach((bucket) => {
+  console.log(bucket.name);
+  });
+})
+.catch((err) => {
+  console.error('ERROR:', err);
+});
+
+
+const fileName = cameraInput.value;
+console.log(fileName);
+// Performs label detection on the local file
+vision.labelDetection({ source: { filename: fileName } })
+.then((results) => {
+  const labels = results[0].labelAnnotations;
+  console.log('Labels:');
+  labels.forEach((label) => console.log(label));
+})
+.catch((err) => {
+  console.error('ERROR:', err);
+});
 
 app.use(bodyParser.json())
 
