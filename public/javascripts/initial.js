28fa9123aa99544b3c77dc0fe027fb656f575d93
Austin Hwang <austin.y.hwang@gmail.com>
2017-12-07 04:09:23 -0500

    backend and file structure change

---
+++ public/javascripts/initial.js
@@ -0,0 +1,51 @@
+/*
+
+This file defines a main method and 
+runs it when the DOM loads
+
+*/
+var fs = require('fs');
+
+document.onreadystatechange = function () {
+
+	//define main method
+	function main() {
+
+		//get required DOM elements
+		var timeBox = document.getElementById('timeBox');
+		var alarmBox = document.getElementById('alarmBox');
+		var alertBox = document.getElementById('alertBox');
+		var selectedPeriodBtn = document.getElementById('amBtn');
+		var periodBtns = document.getElementById('periodBtns');
+		var alarmForm = document.getElementById('alarmForm');
+		var deleteBtn = document.getElementById('deleteBtn');
+
+		//init Data Models
+		var app = new AlarmClockApp();
+		var view = new AlarmClockView(timeBox, alarmBox, alertBox, selectedPeriodBtn);
+		var controller = new AlarmClockController(app, view);
+		
+		//set initial view
+		view.setClockView(app.clock);
+
+		//set event listeners
+		controller.setPeriodBtnListener(periodBtns);
+		controller.setAlarmFormListener(alarmForm);
+		controller.setButtonListener(deleteBtn)
+		
+		//initialize worker
+		var clockWorker = new Worker('javascripts/util/clockWorker.js');
+		controller.setClockWorkerListener(clockWorker);
+		
+		fs.writeFile('/transactions.txt', "", function(err) {
+		});
+	}
+
+	//run main method when DOM loads
+	if (document.readyState === "interactive") {
+		main();
+	}
+}
+
+
+
---
+++ routes/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var stripe = require("stripe")(
+    "sk_test_AiPwVw7Y2qRBP17EZmRftVTK"
+  );
+var bodyParser = require('body-parser')
+var fs = require('fs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+     res.sendFile(path.join(__dirname, '../views', 'index.html'));
+});
+
+
+router.use(bodyParser.urlencoded({ extended: false }))
+
+router.use(bodyParser.json())
+
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+router.get('/', function(req, res, next) {
+    res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+router.post("/charge", function(req, res){
+    stripe.charges.create({
+        amount: req.body.amount,
+        currency: "usd",
+        source: "tok_visa", // obtained with Stripe.js
+        description: req.body.description
+      }, function(err, charge) {
+        // asynchronously called
+      });
+});
+module.exports = router;
